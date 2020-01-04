bc3061e9825a2542d27e80e5e1d91705ce58620c
Wojtek <ciubawojciech@gmail.com>
2020-01-04 22:34:00 +0100

    prepared to deoploy react build

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-const express = require('express')
-var router = express.Router()
-const app = express()
-const port = process.env.PORT || 5000
-var cors = require('cors')
+const express = require("express");
+var router = express.Router();
+const app = express();
+const port = process.env.PORT || 5000;
+var cors = require("cors");
+const path = require("path");
 
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
   console.log("Database connected");
 });
 
-app.use(express.json())
-app.use(cors())
-app.use('/', require('./routes/api/items'));
+app.use(express.json());
+app.use(cors());
+app.use("/", require("./routes/api/items"));
 
-// app.get('/', (req, res) => res.send('Hello World!'));
+if (process.enc.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
