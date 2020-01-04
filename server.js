a639c45e39611e190b674e58694710a587f05f0a
Wojtek <ciubawojciech@gmail.com>
2020-01-04 22:47:05 +0100

    fixed

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/", require("./routes/api/items"));
 
-if (process.enc.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
