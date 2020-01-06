3b8e7dccc53abc32dca278c92cd404f44a1dbd8f
Wojtek <ciubawojciech@gmail.com>
2020-01-06 22:32:12 +0100

    fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var router = express.Router();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 var cors = require("cors");
 const path = require("path");
 
