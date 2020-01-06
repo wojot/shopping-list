65c2b30f0e8a6cd0fb6fc91ab209a78f677d61dd
Wojtek <ciubawojciech@gmail.com>
2020-01-06 20:18:52 +0100

    prepared for production

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,15 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default class App extends Component {
   render() {
