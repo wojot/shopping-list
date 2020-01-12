640afe38aade35831afc3468ae87edd3d691a7ba
Wojtek <ciubawojciech@gmail.com>
2020-01-12 19:49:10 +0100

    fixes

--- server.js
+++ server.js
@@ -2,32 +2,21 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 const path = require("path");
-const config = require('./config/default.json');
+const config = require("./config/default.json");
 app.use(express.json());
 
 var mongoose = require("mongoose");
 
 const dbURL = config.mongoURI;
-// mongoose
-//   .connect(dbURL, { 
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true
-//   })
-//   .then(() => console.log('MongoDB Connected...'))
-//   .catch(err => console.log(err));
+mongoCFG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+};
 
-
-  mongoCFG = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  }
-
-console.log('Attempting to connect to mongoose');
-mongoose.connect(dbURL, mongoCFG) 
+mongoose
+  .connect(dbURL, mongoCFG)
   .then(() => {
     console.log("Connected to Mongo database!");
   })
@@ -35,10 +24,9 @@ mongoose.connect(dbURL, mongoCFG)
     console.error("App starting error:", err.stack);
   });
 
-
 app.use(cors());
 app.use("/api/items", require("./routes/api/items"));
-app.use("/api/users", require("./routes/api/users")); 
+app.use("/api/users", require("./routes/api/users"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
